b1152a9acfde94478a8f1778c2e07b63ff807369
Anton Bobov <abobov@gmail.com>
2021-05-11 21:21:45 +0500

    Updates.

---
+++ files/.vim/templates/=template=.java
@@ -0,0 +1,13 @@
+/*
+ * %FFILE%
+ * Copyright (C) %YEAR% %USER% <%MAIL%>
+ *
+ * Distributed under terms of the %LICENSE% license.
+ */
+
+public class %FILE% {
+  public %FILE%() {
+    %HERE%
+  }
+}
+
